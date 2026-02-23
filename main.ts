5ec2261a258cdd6806afe87e17acc90117d4e547
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-02-23 12:18:38 -0500

    programmed to tell temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Yusuf Ahmed
+ * Created on: Feb 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+let temperature: number
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    basic.showString('The temperature is' + temperature)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+})
