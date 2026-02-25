d0c56f1fdcecf04e10c285224a2e5d18a783ac2f
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-02-25 12:49:53 -0500

    changed and committed

--- main.ts
+++ main.ts
@@ -5,18 +5,15 @@
  * This program tells the temperature
 */
 
-
 let temperature: number
-
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 
-
 input.onButtonPressed(Button.A, function () {
     temperature = input.temperature()
-    basic.showString('The temperature is' + temperature)
+    basic.showString('The temperature is' + temperature + 'C')
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
     basic.pause(1000)
